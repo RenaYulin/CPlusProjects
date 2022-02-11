1c6707d3883a56ca4911560c275fdee9060acd88
RenaYulin <rena_ding89@hotmail.com>
2022-02-11 14:35:48 +1100

    first commit

---
+++ PrintVariable.py
@@ -0,0 +1,14 @@
+x=10
+y="abc"
+print(x)
+print(y)
+print('x')
+print('x= '+str(x))
+
+x, y, z = 100, -45, 0
+print("x=", x, 'y=', y,'z=', z)
+
+a=10
+print('First, varuable a has value', a, 'and type', type(a))
+a='ABC'
+print('Now, variable a has value', a, 'and type', type(a))
